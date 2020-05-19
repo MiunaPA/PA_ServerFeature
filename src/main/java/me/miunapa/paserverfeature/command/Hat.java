7c95fbdbf945cc0496c6f7828dfb59f1821868fe
MiunaPA <fish126732@gmail.com>
2020-05-19 22:13:54 +0800

    增加/hat 穿上界伏盒的警告訊息

--- src/main/java/me/miunapa/paserverfeature/command/Hat.java
+++ src/main/java/me/miunapa/paserverfeature/command/Hat.java
@@ -10,9 +10,13 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Material;
+import java.util.ArrayList;
+import java.util.List;
 import org.bukkit.Bukkit;
 
 public class Hat implements CommandExecutor {
+    List<Material> shulkerBox = new ArrayList<Material>();
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
@@ -30,6 +34,9 @@ public class Hat implements CommandExecutor {
                         TextComponent.fromLegacyText(ChatColor.RED + "你手上物品數量超過一個 無法穿上"));
                 return true;
             }
+            if (shulkerBox.contains(hand.getType())) {
+                player.sendMessage(ChatColor.RED + "請特別注意 不要嘗試直接右鍵打開頭上的界伏盒 有可能會消失(不補償)");
+            }
             inv.setHelmet(hand);
             inv.setItemInMainHand(hat);
             player.updateInventory();
@@ -40,6 +47,23 @@ public class Hat implements CommandExecutor {
     }
 
     public Hat() {
+        shulkerBox.add(Material.SHULKER_BOX);
+        shulkerBox.add(Material.WHITE_SHULKER_BOX);
+        shulkerBox.add(Material.ORANGE_SHULKER_BOX);
+        shulkerBox.add(Material.MAGENTA_SHULKER_BOX);
+        shulkerBox.add(Material.LIGHT_BLUE_SHULKER_BOX);
+        shulkerBox.add(Material.YELLOW_SHULKER_BOX);
+        shulkerBox.add(Material.LIME_SHULKER_BOX);
+        shulkerBox.add(Material.PINK_SHULKER_BOX);
+        shulkerBox.add(Material.GRAY_SHULKER_BOX);
+        shulkerBox.add(Material.LIGHT_GRAY_SHULKER_BOX);
+        shulkerBox.add(Material.CYAN_SHULKER_BOX);
+        shulkerBox.add(Material.PURPLE_SHULKER_BOX);
+        shulkerBox.add(Material.BLUE_SHULKER_BOX);
+        shulkerBox.add(Material.BROWN_SHULKER_BOX);
+        shulkerBox.add(Material.GREEN_SHULKER_BOX);
+        shulkerBox.add(Material.RED_SHULKER_BOX);
+        shulkerBox.add(Material.BLACK_SHULKER_BOX);
         Bukkit.getPluginCommand("hat").setExecutor(this);
     }
 }
