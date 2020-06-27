db9cf1150b610a65213daba7754ff1c013f5a9f2
MiunaPA <fish126732@gmail.com>
2020-06-27 12:32:15 +0800

    /suicide現在需要權限才能使用 paserverfeature.suicide

--- src/main/java/me/miunapa/paserverfeature/command/Suicide.java
+++ src/main/java/me/miunapa/paserverfeature/command/Suicide.java
@@ -21,19 +21,21 @@ public class Suicide extends FeatureStart implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            if (coolDown.containsKey(player.getUniqueId())) {
-                Long interval = System.currentTimeMillis() - coolDown.get(player.getUniqueId());
-                Integer cdTime = config.getInt("SuicideCoolDown");
-                if (interval <= 1000 * cdTime) {
-                    BaseComponent[] baseComponent = TextComponent.fromLegacyText(
-                            "§e指令冷卻中!請等待§c" + (cdTime - (int) (interval / 1000)) + " §e秒");
-                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, baseComponent);
-                    return true;
+            if (player.hasPermission("paserverfeature.suicide")) {
+                if (coolDown.containsKey(player.getUniqueId())) {
+                    Long interval = System.currentTimeMillis() - coolDown.get(player.getUniqueId());
+                    Integer cdTime = config.getInt("SuicideCoolDown");
+                    if (interval <= 1000 * cdTime) {
+                        BaseComponent[] baseComponent = TextComponent.fromLegacyText(
+                                "§e指令冷卻中!請等待§c" + (cdTime - (int) (interval / 1000)) + " §e秒");
+                        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, baseComponent);
+                        return true;
+                    }
                 }
+                player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_DEATH, 1f, 1f);
+                player.setHealth(0.0);
+                coolDown.put(player.getUniqueId(), System.currentTimeMillis());
             }
-            player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_DEATH, 1f, 1f);
-            player.setHealth(0.0);
-            coolDown.put(player.getUniqueId(), System.currentTimeMillis());
         } else {
             sender.sendMessage(ChatColor.RED + "只有玩家可以自殺");
         }
