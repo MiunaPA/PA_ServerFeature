dbd34e5ee49ac1c0790745c381a9a3841695cc29
MiunaPA <fish126732@gmail.com>
2020-09-30 14:21:13 +0800

    新增指定掉落物清除功能 1.8.0

--- src/main/java/me/miunapa/paserverfeature/entity/EntitySpawn.java
+++ src/main/java/me/miunapa/paserverfeature/entity/EntitySpawn.java
@@ -1,12 +1,16 @@
 package me.miunapa.paserverfeature.entity;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -19,11 +23,18 @@ import net.md_5.bungee.api.ChatColor;
 public class EntitySpawn extends SubFeature implements Listener, CommandExecutor {
     Integer pigCount = 0;
     Integer ironGolemCount = 0;
+    List<Material> cleanItemList = new ArrayList<Material>();
 
     public EntitySpawn() {
         super("EntitySpawn");
         pm.registerEvents(this, plugin);
         Bukkit.getPluginCommand("es").setExecutor(this);
+        cleanItemList.add(Material.BAMBOO); // 竹子
+        cleanItemList.add(Material.SUGAR_CANE); // 甘蔗
+        cleanItemList.add(Material.MELON_SLICE); // 西瓜片
+        cleanItemList.add(Material.PUMPKIN); // 南瓜
+        cleanItemList.add(Material.CACTUS); // 仙人掌
+        cleanItemList.add(Material.KELP); // 海帶
     }
 
     public void onDisable() {
@@ -71,10 +82,7 @@ public class EntitySpawn extends SubFeature implements Listener, CommandExecutor
                         e.remove();
                     }
                 }
-                for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-                    player.sendActionBar(ChatColor.RED + "已自動清潔鐵巨人 : " + ChatColor.GREEN
-                            + clearCount.toString());
-                }
+                broadcastActionBar("&c已自動清潔鐵巨人 : &a" + clearCount.toString());
             }
         } else if (event.getEntityType().equals(EntityType.ZOMBIFIED_PIGLIN)) {
             Double random = Math.random();
@@ -104,11 +112,73 @@ public class EntitySpawn extends SubFeature implements Listener, CommandExecutor
                         e.remove();
                     }
                 }
-                for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-                    player.sendActionBar(ChatColor.RED + "已自動清潔殭屍豬布林 : " + ChatColor.GREEN
-                            + clearCount.toString());
+                broadcastActionBar("&c已自動清潔殭屍豬布林 : &a" + clearCount.toString());
+                if (config.getBoolean("EntitySpawn.CleanItem")) {
+                    Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
+                        @Override
+                        public void run() {
+                            cleanItemProcess();
+                        }
+                    }, 400);
                 }
             }
+        }
+    }
+
+    void cleanItemProcess() {
+        Integer count = 0;
+        for (World w : Bukkit.getWorlds()) {
+            for (Entity e : w.getEntities()) {
+                if (e.getType() == EntityType.DROPPED_ITEM) {
+                    Item item = (Item) e;
+                    if (cleanItemList.contains(item.getItemStack().getType())) {
+                        count += 1;
+                    }
+                }
+            }
+        }
+        if (count >= 400) {
+            broadcastActionBar("&1偵測到掉落物過多!&7(" + count + ") &4即將在 30 秒後清除地板上的指定掉落物");
+            Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
+                @Override
+                public void run() {
+                    broadcastActionBar("&4即將在 20 秒後清除地板上的指定掉落物");
+                }
+            }, 200);
+            Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
+                @Override
+                public void run() {
+                    broadcastActionBar("&4即將在 10 秒後清除地板上的指定掉落物");
+                }
+            }, 400);
+            Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
+                @Override
+                public void run() {
+                    cleanItem();
+                }
+            }, 600);
+        }
+    }
+
+    void cleanItem() {
+        Integer cleanCount = 0;
+        for (World w : Bukkit.getWorlds()) {
+            for (Entity e : w.getEntities()) {
+                if (e.getType() == EntityType.DROPPED_ITEM) {
+                    Item item = (Item) e;
+                    if (cleanItemList.contains(item.getItemStack().getType())) {
+                        item.remove();
+                        cleanCount += 1;
+                    }
+                }
+            }
+        }
+        broadcastActionBar("&d已清除指定掉落物 共 &c" + cleanCount + " &d個");
+    }
+
+    void broadcastActionBar(String text) {
+        for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+            player.sendActionBar(ChatColor.translateAlternateColorCodes('&', text));
         }
     }
 }
