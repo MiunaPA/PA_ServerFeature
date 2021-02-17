1ccc754b7c981a9cc04645ed31a627245b510fcb
MiunaPA <fish126732@gmail.com>
2021-02-17 16:16:07 +0800

    增加不可放置

--- src/main/java/me/miunapa/paserverfeature/feature/DisableNetheriteBreak.java
+++ src/main/java/me/miunapa/paserverfeature/feature/DisableNetheriteBreak.java
@@ -4,6 +4,7 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import me.miunapa.paserverfeature.SubFeature;
 import net.md_5.bungee.api.ChatColor;
 
@@ -26,6 +27,15 @@ public class DisableNetheriteBreak extends SubFeature implements Listener {
                 || event.getBlock().getType() == Material.ANCIENT_DEBRIS) {
             event.getPlayer().sendMessage(ChatColor.RED + "本伺服的地獄不可挖掘遠古遺骸! 請至資源服挖掘");
             event.setDropItems(false);
+        }
+    }
+
+    @EventHandler
+    public void onInventoryOpenEvent(BlockPlaceEvent event) {
+        if (event.getBlock().getWorld().getName().equals("world_nether")
+                || event.getBlock().getType() == Material.ANCIENT_DEBRIS) {
+            event.getPlayer().sendMessage(ChatColor.RED + "本伺服的地獄不可放下遠古遺骸! 請別在地獄放");
+            event.setCancelled(true);
         }
     }
 }
